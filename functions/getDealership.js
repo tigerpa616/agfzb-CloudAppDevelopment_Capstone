f19b33be57b33259b031b133db248bf4cebc377f
Daniel <57764947+tigerpa616@users.noreply.github.com>
2021-10-08 04:10:40 -0400

    Update getDealership.js

--- functions/getDealership.js
+++ functions/getDealership.js
@@ -7,14 +7,18 @@
 
  async function main(params) {
      const cloudant = Cloudant({
-         url: params.COUCH_URL,
-         plugins: { iamauth: { iamApiKey: params.IAM_API_KEY } }
+         url: "https://f29fb6c8-981f-4cc0-b09b-9cca05f47b9d-bluemix.cloudantnosqldb.appdomain.cloud",
+         plugins: { iamauth: { iamApiKey: "Un0dOUFr88tJze2BpfAveXsLYYozRoAKN6a8zLiXz-EC" } }
      });
  
  
      try {
-         let dbList = await cloudant.db.list();
-         return { "dealerships": dbList };
+         //selecting cloudant dbs
+         const db = cloudant.db.use("dealerships");
+          let dbList = await db.list({include_docs:true});
+         return { "dbs": dbList };
+         
+         
      } catch (error) {
          return { error: error.description };
      }
