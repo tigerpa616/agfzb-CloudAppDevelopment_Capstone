c132d6c01b31e9dfbdf7ac52438dac340386666f
Daniel <57764947+tigerpa616@users.noreply.github.com>
2021-10-18 01:19:42 -0400

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,26 +1,45 @@
 import requests
 import json
+import logging
 # import related models here
 from requests.auth import HTTPBasicAuth
+from . import models
+from ibm_watson import NaturalLanguageUnderstandingV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from ibm_watson.natural_language_understanding_v1 import Features, SentimentOptions
 
-
+logger = logging.getLogger(__name__)
 # Create a `get_request` to make HTTP GET requests
 # e.g., response = requests.get(url, params=params, headers={'Content-Type': 'application/json'},
 #                                     auth=HTTPBasicAuth('apikey', api_key))
-
 def get_request(url, **kwargs):
-    """ Get """
+    print(kwargs)
+    print("GET from {} ".format(url))
     try:
-        response = requests.get(
-            url, headers={'Content-Type': 'application/json'}, params=kwargs)
+        # Call get method of requests library with URL and parameters
+        response = requests.get(url, headers={'Content-Type': 'application/json'}, params=kwargs)
     except:
+        # If any error occurs
         print("Network exception occurred")
+    status_code = response.status_code
+    print("With status {} ".format(status_code))
     json_data = json.loads(response.text)
     return json_data
 # Create a `post_request` to make HTTP POST requests
+def post_request(url, json_payload, **kwargs):
+    json_obj = json_payload["review"]
+    print(kwargs)
+    try:
+        response = requests.post(url, json=json_obj, params=kwargs)
+    except:
+        print("Something went wrong")
+    print (response)
+    return response
 # e.g., response = requests.post(url, params=kwargs, json=payload)
 
-def get_dealerships_from_cf():
+
+# Create a get_dealers_from_cf method to get dealers from a cloud function
+def get_dealers_from_cf(url, **kwargs):
     results = []
     # Call get_request with a URL parameter
     json_result = get_request(url)
@@ -38,6 +57,7 @@ def get_dealerships_from_cf():
             results.append(dealer_obj)
             print('-------------------------------------------------------')
     return results
+
 
 # Create a get_dealer_reviews_from_cf method to get reviews by dealer id from a cloud function
 def get_dealer_reviews_by_id_from_cf(url, dealerId):
@@ -63,13 +83,31 @@ def get_dealer_reviews_by_id_from_cf(url, dealerId):
             results.append(review_obj)
 
     return results
-
+# - Call get_request() with specified arguments
+# - Parse JSON results into a DealerView object list
 
 
 # Create an `analyze_review_sentiments` method to call Watson NLU and analyze text
-# def analyze_review_sentiments(text):
-# - Call get_request() with specified arguments
-# - Get the returned sentiment label such as Positive or Negative
-
-
-
+def analyze_review_sentiments(text):
+    api_key = "UNL1ShV-c2bAwfGGyqQFjE-qVOOE-yPskbDybk8CWro0"
+    url = "https://api.us-south.natural-language-understanding.watson.cloud.ibm.com/instances/1bcae669-efa9-4160-9535-18d3807000a3"
+    texttoanalyze= text
+    version = '2020-08-01'
+    authenticator = IAMAuthenticator(api_key)
+    natural_language_understanding = NaturalLanguageUnderstandingV1(
+    version='2020-08-01',
+    authenticator=authenticator
+    )
+    natural_language_understanding.set_service_url(url)
+    response = natural_language_understanding.analyze(
+        text=text,
+        features= Features(sentiment= SentimentOptions())
+    ).get_result()
+    print(json.dumps(response))
+    sentiment_score = str(response["sentiment"]["document"]["score"])
+    sentiment_label = response["sentiment"]["document"]["label"]
+    print(sentiment_score)
+    print(sentiment_label)
+    sentimentresult = sentiment_label
+    
+    return sentimentresult
